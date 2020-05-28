30727c20875d970a29cc02d30e809599e8b44d57
JoaoRodrigoGoncalves <joaorvdc@hotmail.com>
2020-05-28 13:02:53 +0100

    Restructured API Endpoints
    New Structure gave to the endpoints and files were renamed

---
+++ api/5.0.0/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+public class API_Settings{
+
+    public class FileUpload{
+
+        $filesPath = "resources/usercontent/"; // path staring on main folder (Summaries)
+        $maxFileSize = "52428800"; // in bytes (~52MB)
+
+    }
+
+}
+
+?>
